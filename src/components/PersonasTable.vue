dfc0e27da090cb1152fdd0790ffd025dd90b714b
juanjo2702 <juanjo270201@gmail.com>
2026-07-20 23:26:14 -0400

    optimizacion UI y lazy loading de imagenes

--- src/components/PersonasTable.vue
+++ src/components/PersonasTable.vue
@@ -1,17 +1,29 @@
 <template>
-  <q-table :rows="personas" :columns="columns" row-key="propietario_id">
+  <q-table :rows="personas" :columns="columns" row-key="propietario_id" flat bordered class="shadow-2 rounded-borders">
     <!-- Imagen de la persona -->
     <template v-slot:body-cell-foto="props">
       <q-td align="center">
-        <img v-if="props.row.foto" :src="getStorageUrl(props.row.foto)" style="max-height: 100px;" />
-        <span v-else>No image available</span>
+        <q-avatar size="60px" class="shadow-1" v-if="props.row.foto">
+          <q-img :src="getStorageUrl(props.row.foto)" loading="lazy" spinner-color="primary" style="height: 100%; width: 100%; object-fit: cover;">
+            <template v-slot:error>
+              <div class="absolute-full flex flex-center bg-negative text-white text-caption">
+                <q-icon name="broken_image" size="24px" />
+              </div>
+            </template>
+          </q-img>
+        </q-avatar>
+        <q-avatar size="60px" color="grey-3" text-color="grey-7" icon="person" v-else />
       </q-td>
     </template>
     <!-- Acciones: Editar y Eliminar -->
     <template v-slot:body-cell-actions="props">
       <q-td align="center">
-        <q-btn flat icon="edit" color="primary" @click="editPersona(props.row)" />
-        <q-btn flat icon="delete" color="negative" @click="deletePersona(props.row)" />
+        <q-btn round flat icon="edit" color="primary" @click="editPersona(props.row)">
+          <q-tooltip>Editar Persona</q-tooltip>
+        </q-btn>
+        <q-btn round flat icon="delete" color="negative" @click="deletePersona(props.row)">
+          <q-tooltip>Eliminar Persona</q-tooltip>
+        </q-btn>
       </q-td>
     </template>
   </q-table>
